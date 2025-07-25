3a9ee392d7f32209ca8e2880f6283f4867c8778b
Parth Shah <shahpv@ATRI003362.local>
2025-07-25 10:04:52 -0700

    test(React-Training): Added a test for the edit functionality
    Updated the dashboard page test script to test out the workflow to edit the last transaction.
    
    [ICTRAINING-2205]

--- cypress/e2e/dashboard_page.cy.ts
+++ cypress/e2e/dashboard_page.cy.ts
@@ -1,9 +1,11 @@
 describe("Expense Tracker Dashboard Page Tests", () => {
   const jsonServerUrl = "http://localhost:3030/transactions";
   beforeEach(() => {
+    // Reset the database before each test and intercepting the GET request
     cy.intercept("GET", jsonServerUrl, { fixture: "transactions.json" }).as(
       "getTransactions"
     );
+    // Intercept the POST request to create a new transaction
     cy.intercept("POST", jsonServerUrl, (req) => {
       req.reply({
         statusCode: 201,
@@ -45,14 +47,20 @@ describe("Expense Tracker Dashboard Page Tests", () => {
     cy.get('input[name="category"]').type("Food");
 
     cy.get("Button").contains("Add a Transaction").click();
+
+    // Verify that the POST request was made with the correct data
     cy.wait("@postTransactions").then((interception) => {
       expect(interception.response.statusCode).to.eq(201);
       expect(interception.request.body.amount).to.eq(112);
       expect(interception.request.body.date).to.eq("2023-10-01");
       expect(interception.request.body.type).to.eq("expense");
       expect(interception.request.body.category).to.eq("Food");
+      expect(interception.response.body.id).to.eq(5);
     });
+
     cy.url().should("include", "/dashboard");
+
+    // Verify that the new transaction is in the table
     cy.wait("@getTransactions");
 
     cy.get("table tbody tr").should("have.length", 5);
@@ -63,6 +71,59 @@ describe("Expense Tracker Dashboard Page Tests", () => {
         cy.contains("2023-10-01");
         cy.contains("expense");
         cy.contains("Food");
+      });
+  });
+
+  it("should allow for editing transactions", () => {
+    const lastTransactionId = 4;
+    const newAmount = 150;
+    const newDate = "2023-11-02";
+    cy.get("table tbody tr")
+      .last()
+      .within(() => {
+        cy.get("Button").contains("Edit").should("exist");
+        cy.get("Button").contains("Edit").click();
+      });
+
+    // User should see a offcanvas with the title "Edit Transaction"
+    cy.get("div[class='offcanvas-header']")
+      .should("exist")
+      .should("contain", "Edit Transaction");
+
+    // Updating the amount and date of the transaction
+    cy.get('input[name="amount"]').clear().type(newAmount.toString());
+    cy.get('input[name="date"]').clear().type(newDate);
+
+    // Intercept the PUT request to update the transaction
+    cy.intercept("PUT", `${jsonServerUrl}/${lastTransactionId}`, (req) => {
+      req.reply({
+        statusCode: 200,
+        body: {
+          id: lastTransactionId,
+          amount: newAmount,
+          date: newDate,
+        },
+      });
+    }).as("putTransaction");
+
+    // Submitting the form
+    cy.get("Button").contains("Save Changes").should("exist");
+    cy.get("Button").contains("Save Changes").click();
+
+    // Verify that the PUT request was made with the correct data
+    cy.wait("@putTransaction").then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+      expect(interception.request.body.amount).to.eq(newAmount);
+      expect(interception.request.body.date).to.eq(newDate);
+    });
+
+    // Verify that the user is redirected back to the dashboard and the changes are reflected
+    cy.url().should("include", "/dashboard");
+    cy.get("table tbody tr")
+      .last()
+      .within(() => {
+        cy.contains(newAmount);
+        cy.contains(newDate);
       });
   });
 });
